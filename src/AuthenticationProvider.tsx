a8e1be0a7ffc360e8284686b126e33b123f4eed8
Ulad Kasach <uladkasach@gmail.com>
2023-05-29 04:23:13 -0400

    fix(lifecycle): ensure that subscribing to events occurs before first events possible

--- src/AuthenticationProvider.tsx
+++ src/AuthenticationProvider.tsx
@@ -1,7 +1,7 @@
-import React, { ReactChild, ReactChildren, useEffect } from 'react';
+import React, { ReactChild, ReactChildren, useEffect, useState } from 'react';
 
 import { AuthenticationContext } from './AuthenticationContext';
-import { AuthableTokenUpdatedEvent, authableTokenUpdatedEventStream } from './token/authableTokenUpdatedEventStream';
+import { AuthableTokenUpdatedEvent, authableTokenUpdatedEventStream, AuthableTokenUpdatedEventType } from './token/authableTokenUpdatedEventStream';
 import { useAuthableTokenClaims } from './token/useAuthableTokenClaims';
 
 /**
@@ -13,13 +13,19 @@ import { useAuthableTokenClaims } from './token/useAuthableTokenClaims';
 const useTokenLifecycleEvents = ({ onTokenForgotten }: { onTokenForgotten?: () => void }) => {
   // define what to do when we consume a token updated event
   const consumer = ({ event }: { event: AuthableTokenUpdatedEvent }): void => {
-    if (onTokenForgotten) onTokenForgotten();
+    if (event.type === AuthableTokenUpdatedEventType.FORGOT && onTokenForgotten) onTokenForgotten();
   };
+
+  // track whether we've already subscribed once (do so in the default of a hook, since apparently those run before useEffects)
+  useState(() => {
+    // subscribe the auth token updated events, so we have the latest state of the token at all time
+    authableTokenUpdatedEventStream.subscribe({ consumer }); // subscribe on mount; would do inside useEffect, but it runs after the other setState default hook which wipes out the token, and we need to listen to that occurance, so we need to run first; this is an unfortunate react quirk
+  });
 
   // subscribe to token updated events event stream (in useEffect, so that its only done once)
   useEffect(() => {
     // subscribe the auth token updated events, so we have the latest state of the token at all time
-    authableTokenUpdatedEventStream.subscribe({ consumer }); // subscribe on mount
+    // NOTE: we actually subscribed in the useState default above, since apparently all useState defaults run before useEffects; since we need to consume the event produced by `useAuthableTokenClaims.tsx::setState(getAuthableToken...)`, we must subscribe in useState too
 
     // and ensure that we unsubscribe on unmount to cleanup after ourselves
     return () => authableTokenUpdatedEventStream.unsubscribe({ consumer }); // unsubscribe on unmount
@@ -62,11 +68,11 @@ export const AuthenticationProvider = ({
     onTokenForgotten?: () => void;
   };
 }) => {
+  // listen to token events
+  useTokenLifecycleEvents({ onTokenForgotten: listen?.onTokenForgotten });
+
   // grab the claims
   const { claims } = useAuthableTokenClaims();
-
-  // use token events
-  useTokenLifecycleEvents({ onTokenForgotten: listen?.onTokenForgotten });
 
   // pass them to the provider
   return <AuthenticationContext.Provider value={{ claims, directoryUuid, clientUuid }}>{children}</AuthenticationContext.Provider>;
